f5969877c6c18264c868812d35ced2212915e7f4
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-05-23 01:30:58 +0300

    Clean up unused code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,12 @@
 import { getHeader } from './header';
 import { getFooter } from './footer';
 import { getPhotosGallery } from './photos';
-import { getPhotoView } from './photo';
-
-const routes = [
-  {
-    'path': '/',
-    'component': async () => await getPhotosGallery()
-  },
-  {
-    'path': '/photo',
-    'component': async () => await getPhotoView()
-  }
-];
 
 document.body.appendChild(getHeader());
 const main = document.createElement('main');
-main.appendChild(document.createElement('div'));
+main.appendChild(await getPhotosGallery());
 document.body.appendChild(main);
 document.body.appendChild(getFooter());
-
-window.router = async path => {
-  window.history.pushState({}, '', path);
-
-  for (const route of routes) {
-    if (route.path === path) {
-      document.querySelector('main > div')
-        .replaceWith(await route.component());
-      break;
-    }
-
-    const notfound = document.createElement('h1');
-    notfound.textContent = 'ups, not found';
-    document.querySelector('main > div')
-      .replaceWith(await route.component());
-  }
-};
-
-window.router('/')
-  .then(r => console.log('Hello!'));
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
--- src/photo.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-
-export async function getPhotoView() {
-  const container = document.createElement('div');
-  container.className = 'container';
-  container.innerHTML = '<p>Photo view</p>'
-  return container;
-}
